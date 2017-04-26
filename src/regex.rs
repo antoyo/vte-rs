c1dde6662014c7cc2bb35b5eb58d94d59d9d16bf
Antoni Boucher <bouanto@zoho.com>
2017-04-25 20:56:19 -0400

    Fix Regex constructors

--- src/regex.rs
+++ src/regex.rs
@@ -3,15 +3,14 @@ use std::ptr;
 use ffi;
 use glib::translate::{ToGlibPtr, from_glib_full, from_glib_none};
 use gtk::Error;
-use libc;
 
 use Regex;
 
 impl Regex {
-    pub fn new_for_match(pattern: &str, pattern_length: libc::c_long, flags: u32) -> Result<Regex, Error> {
+    pub fn new_for_match(pattern: &str, flags: u32) -> Result<Regex, Error> {
         let mut error = ptr::null_mut();
         unsafe {
-            let regex = ffi::vte_regex_new_for_match(pattern.to_glib_none().0, pattern_length as _, flags, &mut error);
+            let regex = ffi::vte_regex_new_for_match(pattern.to_glib_none().0, pattern.len() as _, flags, &mut error);
             if error.is_null() {
                 Ok(from_glib_none(regex))
             }
@@ -21,10 +20,10 @@ impl Regex {
         }
     }
 
-    pub fn new_for_search(pattern: &str, pattern_length: libc::c_long, flags: u32) -> Result<Regex, Error> {
+    pub fn new_for_search(pattern: &str, flags: u32) -> Result<Regex, Error> {
         let mut error = ptr::null_mut();
         unsafe {
-            let regex = ffi::vte_regex_new_for_search(pattern.to_glib_none().0, pattern_length as _, flags, &mut error);
+            let regex = ffi::vte_regex_new_for_search(pattern.to_glib_none().0, pattern.len() as _, flags, &mut error);
             if error.is_null() {
                 Ok(from_glib_none(regex))
             }
