4a8faf8244ce418a2f0f84ec3df80ce0388e404d
Thomas Kluyver <thomas@kluyver.me.uk>
2017-06-01 21:58:39 +0100

    Fix passing pointers to ffi functions

--- src/terminal.rs
+++ src/terminal.rs
@@ -9,47 +9,56 @@ use glib::translate::*;
 use glib_ffi;
 use Terminal;
 
+macro_rules! option_to_ptr {
+    ($e:expr) => {
+        match $e {
+            Some(value) => value.to_glib_none().0,
+            None => ptr::null()
+        }
+    };
+}
+
 impl Terminal {
     pub fn set_color_background(&self, background: &gdk::RGBA) {
         unsafe {
-            ffi::vte_terminal_set_color_background(self.to_glib_none().0, background);
+            ffi::vte_terminal_set_color_background(self.to_glib_none().0, background.to_glib_none().0);
         }
     }
 
     pub fn set_color_bold(&self, bold: Option<&gdk::RGBA>) {
         unsafe {
-            ffi::vte_terminal_set_color_bold(self.to_glib_none().0, option_to_ptr(bold));
+            ffi::vte_terminal_set_color_bold(self.to_glib_none().0, option_to_ptr!(bold));
         }
     }
 
     pub fn set_color_cursor(&self, cursor_background: Option<&gdk::RGBA>) {
         unsafe {
-            ffi::vte_terminal_set_color_cursor(self.to_glib_none().0, option_to_ptr(cursor_background));
+            ffi::vte_terminal_set_color_cursor(self.to_glib_none().0, option_to_ptr!(cursor_background));
         }
     }
 
     #[cfg(feature = "v0_44")]
     pub fn set_color_cursor_foreground(&self, cursor_foreground: Option<&gdk::RGBA>) {
         unsafe {
-            ffi::vte_terminal_set_color_cursor_foreground(self.to_glib_none().0, option_to_ptr(cursor_foreground));
+            ffi::vte_terminal_set_color_cursor_foreground(self.to_glib_none().0, option_to_ptr!(cursor_foreground));
         }
     }
 
     pub fn set_color_foreground(&self, foreground: &gdk::RGBA) {
         unsafe {
-            ffi::vte_terminal_set_color_foreground(self.to_glib_none().0, foreground);
+            ffi::vte_terminal_set_color_foreground(self.to_glib_none().0, foreground.to_glib_none().0);
         }
     }
 
     pub fn set_color_highlight(&self, highlight_background: Option<&gdk::RGBA>) {
         unsafe {
-            ffi::vte_terminal_set_color_highlight(self.to_glib_none().0, option_to_ptr(highlight_background));
+            ffi::vte_terminal_set_color_highlight(self.to_glib_none().0, option_to_ptr!(highlight_background));
         }
     }
 
     pub fn set_color_highlight_foreground(&self, highlight_foreground: Option<&gdk::RGBA>) {
         unsafe {
-            ffi::vte_terminal_set_color_highlight_foreground(self.to_glib_none().0, option_to_ptr(highlight_foreground));
+            ffi::vte_terminal_set_color_highlight_foreground(self.to_glib_none().0, option_to_ptr!(highlight_foreground));
         }
     }
 
@@ -62,12 +71,5 @@ impl Terminal {
                 glib_ffi::G_SPAWN_DEFAULT, None, ptr::null_mut(), None, -1, ptr::null_mut(),
                 None, ptr::null_mut());
         }
-    }
-}
-
-fn option_to_ptr<T>(value: Option<&T>) -> *const T {
-    match value {
-        Some(value) => value as *const _,
-        None => ptr::null(),
     }
 }
